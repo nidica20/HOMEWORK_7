e962cfd77c6315549b0a99f182c7f08e1d1456c9
nidica20 <45465822+nidica20@users.noreply.github.com>
2019-06-24 22:09:29 -0700

    Add files via upload

---
+++ homework7/headers/LandingQueue.h
@@ -0,0 +1,56 @@
+#ifndef LANDING_Q_H_
+#define LANDING_Q_H_
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include "Plane.h"
+#include "Random.h"
+
+extern Random my_random;  // Enables us to access the global variable declared in Simulator.h
+
+class LandingQueue
+{
+private:
+	double arrival_rate;            // plane arrival rate per minute
+	std::queue<Plane *> the_queue;  // queue of planes in the landing queue
+	int total_wait;  // total accumulated wait time in the landing queue
+	int num_served;  // number of planes served through the landing queue
+
+public:
+	LandingQueue() : total_wait(0), num_served(0) {}
+
+	void set_arrival_rate(double arrival_rate) {
+		this->arrival_rate = arrival_rate;
+	}
+
+	int get_total_wait() {
+		return total_wait;
+	}
+
+	int get_num_served() {
+		return num_served;
+	}
+
+	void update(int clock) 
+	{
+		/* FIXME:  add a new plane into the landing queue based on the arrival_rate
+		   HINT: my_random.next_double() returns a random value between 0 and 1.
+		         Read how the the simulation described in the book does this 
+		         (section 6.5 pages. 392-393).
+		         Look at the Passenger_Queue::check_new_arrival() method 
+		*/
+
+		if (my_random.next_double() < arrival_rate) //condition compares psuedoraondom number to arrival_rate
+		{
+			the_queue.push(new Plane(clock));
+			//since values are being compared, the probability of condition being true is proportional to arrival_rate value
+			// If arrival_rate is 0.4, then condition is true 40% of time (as described on page 393).
+		}
+
+	}
+
+	friend class ServiceQueue;
+};
+
+#endif
